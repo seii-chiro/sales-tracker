2057fc5834c3128408911861b14eed78c7a15900
Rhay from Workstation <javier.johnrhay@gmail.com>
2026-03-16 10:28:25 +0800

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
-import { useEffect, useState } from 'react'
+
 import './App.css'
-import { supabase } from './supabaseClient'
-import type { InventoryItem } from './types/inventory_items'
+import { createBrowserRouter, RouterProvider } from "react-router"
+import Sales from './pages/Sales'
+import Inventory from './pages/Inventory'
+import Home from './pages/Home'
+import RootLayout from './layouts/RootLayout'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "inventory",
+        element: <Inventory />
+      },
+      {
+        path: "sales",
+        element: <Sales />
+      }
+    ]
+  }
+])
 
 function App() {
-  const [inventoryItems, setInventoryItems] = useState<InventoryItem[]>([])
 
-  useEffect(() => {
-    const fetchInventoryItems = async () => {
-      const { data, error } = await supabase
-        .from('inventory_items')
-        .select('*')
-      if (error) {
-        console.error('Error fetching inventory items:', error)
-      } else {
-        setInventoryItems(data)
-      }
-    }
-    fetchInventoryItems()
-  }, [])
 
-  console.log(inventoryItems)
-
-  return (
-    <div>
-      <h1 className='text-red-600'>Vite + React</h1>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
